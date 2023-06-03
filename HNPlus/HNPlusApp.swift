d5a53bf0edcda8001613271fd35ecc300abefcc0
asnewman <ashznewman@gmail.com>
2023-06-03 10:13:21 -0700

    Initial Commit

---
+++ HNPlus/HNPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HNPlusApp.swift
+//  HNPlus
+//
+//  Created by Ashley Newman on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct HNPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
